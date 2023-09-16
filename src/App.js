f6cb81767d16364e8f89aaafe28fa553655ee8a8
Warat Supaporn <supaporn.dev@gmail.com>
2023-09-16 13:34:50 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  // create state for set count
+
+  const [students,setStudent] = useState([
+    {id:1,name:"Nook"},
+    {id:2,name:"Kwang"},
+    {id:3,name:"News"},
+    {id:4,name:"Guess"}
+  ]);
+
+  const [show,setShow] = useState(true); 
+  // if ture display content, false hide content
+
+  function deleteStudent(id){
+    setStudent(students.filter(item=>item.id !== id))
+  }
+  
+  // Display area
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+       <h1>Number of students : {students.length}</h1>
+       <h2>{show}</h2>
+       <button onClick={()=>setShow(!show)}>Toggle</button>
+       <ul>
+        {show && students.map((item)=>(
+          <li key = {item.id}>
+            <p>{item.id} - {item.name}</p>
+            <button onClick = {()=>deleteStudent(item.id)}>Delete</button>
+          </li>
+        ))}
+       </ul>
+
+       <input type="text" />
+       <button onClick = {() => setStudent()}>Add</button>
+    </>
   );
 }
 
